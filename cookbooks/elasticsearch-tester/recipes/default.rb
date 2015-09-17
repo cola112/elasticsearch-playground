ebc19efe1b21f540afba59baadec86af1066035a
sardo.ip <sardo.ip@nxtomo.hk>
2015-09-17 15:11:49 +0800

    Can auto select yes when install java and fix the sum of deb file

--- cookbooks/elasticsearch-tester/recipes/default.rb
+++ cookbooks/elasticsearch-tester/recipes/default.rb
@@ -4,13 +4,15 @@ bash 'Install Java' do
     apt-get install python-software-properties -y
 	add-apt-repository ppa:webupd8team/java -y
 	apt-get update
-	sudo apt-get install oracle-java8-installer -y
+    echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+    echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+    sudo apt-get -y install oracle-java7-installer
   EOC
 end
 
 remote_file '/tmp/elasticsearch.deb' do
     source 'https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.2.deb'
-    checksum '1c78da90889ad87c55315dcfe3ff3eca88115791'
+    checksum '791fb9f2131be2cf8c1f86ca35e0b912d7155a53f89c2df67467ca2105e77ec2'
     notifies :run, 'execute[run_install-elasticsearch]', :immediately
 end
 
