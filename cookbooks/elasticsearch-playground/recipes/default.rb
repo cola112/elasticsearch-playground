620d573871f28f25623c3d40bd1eed508bef7a08
Daniel Leung <daniel.leung@MW-GAMM103024.local>
2015-09-23 15:27:26 +0800

    update recipe

--- cookbooks/elasticsearch-playground/recipes/default.rb
+++ cookbooks/elasticsearch-playground/recipes/default.rb
@@ -2,8 +2,8 @@ bash 'Install Java' do
   user 'root'
   code <<-EOC
     apt-get install python-software-properties -y
-	add-apt-repository ppa:webupd8team/java -y
-	apt-get update
+    add-apt-repository ppa:webupd8team/java -y
+    apt-get update
     echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
     echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
     sudo apt-get -y install oracle-java7-installer
